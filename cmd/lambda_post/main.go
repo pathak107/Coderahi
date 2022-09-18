1b444be48d5c05b440b8c46b8b50b458d13e050c
pathak107 <shubhampathak1428@gmail.com>
2022-09-19 00:50:48 +0530

    post lambda working

---
+++ cmd/lambda_post/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/pathak107/coderahi-learn/pkg/handler"
+	"github.com/pathak107/coderahi-learn/pkg/services"
+)
+
+func HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	db, err := services.NewDatabaseService()
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+	h := handler.NewHandler(db)
+
+	ApiResponse := events.APIGatewayProxyResponse{}
+	// Switch for identifying the HTTP request
+	switch request.HTTPMethod {
+	case "GET":
+		if request.PathParameters["post_id"] != "" {
+			data, err := h.FindPostByID(request.PathParameters["post_id"])
+			if err != nil {
+				return handler.HandleError(err), nil
+			}
+			ApiResponse = events.APIGatewayProxyResponse{Body: data, StatusCode: 200}
+		} else {
+			data, err := h.FindAllPosts()
+			if err != nil {
+				return handler.HandleError(err), nil
+			}
+			ApiResponse = events.APIGatewayProxyResponse{Body: data, StatusCode: 200}
+		}
+
+	case "POST":
+		//validates json and returns error if not working
+		// err := fastjson.Validate(request.Body)
+		data, err := h.CreatePost(request.Body)
+		if err != nil {
+			return handler.HandleError(err), nil
+		}
+		ApiResponse = events.APIGatewayProxyResponse{Body: data, StatusCode: 200}
+	case "DELETE":
+		data, err := h.DeletePost(request.PathParameters["post_id"])
+		if err != nil {
+			return handler.HandleError(err), nil
+		}
+		ApiResponse = events.APIGatewayProxyResponse{Body: data, StatusCode: 200}
+
+	case "PATCH":
+		data, err := h.EditPost(request.Body, request.PathParameters["post_id"])
+		if err != nil {
+			return handler.HandleError(err), nil
+		}
+		ApiResponse = events.APIGatewayProxyResponse{Body: data, StatusCode: 200}
+	}
+	// Response
+	return ApiResponse, nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
